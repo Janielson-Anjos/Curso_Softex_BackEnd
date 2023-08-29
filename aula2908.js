ae6fb5d08aab4dc53b5f75b1eab2f5d06d3b8bef
Janielson Anjos <72242469+Janielson-Anjos@users.noreply.github.com>
2023-08-29 16:39:47 -0300

    Add files via upload
    add aula 29/08

---
+++ aula2908.js
@@ -0,0 +1,128 @@
+const readline = require("readline-sync");
+
+// const livro = {
+//   titulo: "1984",
+//   autor: "George Orwell",
+//   isDisponivel: false,
+
+//   checkin: function () {
+//     this.isDisponivel = true;
+//   },
+//   checkout: function () {
+//     this.isDisponivel = false;
+//   },
+// };
+
+// const rodar = true;
+
+// while (rodar == true) {
+//   const pergunta1 = readline.question("digite o nome do autor: ");
+//   if (pergunta1 == livro.autor) {
+//     if(isDisponivel == true){
+//         console.log(`o livro`);
+//     }
+//   }
+// }
+
+/////////////////////////////////// LISTA DE EXERCICIO /////////////////////////////////////////
+
+// const pessoa = {
+//     nome: "João",
+
+// }
+
+// const usuario = {
+//     idade: 18
+// }
+
+// console.log(usuario.idade);
+
+// const produto = {
+//   preco: 50,
+// };
+
+// produto.preco = 75;
+// console.log(produto.preco);
+
+// const contato = {
+//   email: "abc@email.com",
+//   nome: "eu",
+// };
+
+// delete contato.email;
+// console.log(contato);
+
+// const cliente = {
+//   nome: "eu",
+// };
+
+// cliente.endereco = "endereco";
+// console.log(cliente);
+
+// const empresa = {
+//   departamentos: ["Financeiro", "RH", "suporte"],
+// };
+
+// console.log(empresa.departamentos[0]);
+
+// let aluno = {
+//   notas: [85, 90, 78],
+// };
+
+// aluno.notas[1] = 95;
+
+// console.log(aluno.notas);
+
+// let dados = {
+//   valores: [10, 20, 30],
+// };
+
+// for (let i = 0; i < dados.valores.length; i++) {
+//   if (dados.valores[i] === 20) {
+//     dados.valores.splice(i, 1);
+//     // delete dados.valores[i];
+//   }
+// }
+// console.log(dados.valores);
+
+// let biblioteca = {
+//   titulo: "livro",
+//   autor: "Maria",
+// };
+
+//////////////////////////////  Lista de Enumerates ////////////////////
+
+let diasDaSemana = {
+  domingo: "Domingo",
+  segunda: "Segunda",
+  terca: "Terça",
+  quarta: "Quarta",
+  quinta: "Quinta",
+  sexta: "Sexta",
+  sabado: "Sabado",
+};
+
+let inimigos = {
+  lvl1: "Ogro",
+  lvl2: "Urso",
+  lvl3: "Caveira",
+  lvl4: "Fantasma",
+};
+
+let tiposDeClasse = {
+  eco: "Econômica",
+  exe: "Executiva",
+  pclasse: "Primeira Classe",
+};
+
+let tarefas = {
+  trabalho: "Trabalho",
+  estudo: "Estudo",
+  lazer: "Lazer",
+};
+
+let statusPedido = {
+  emProcessamento: "Em Processamento",
+  enviado: "Enviado",
+  entregue: "Entregue",
+};
