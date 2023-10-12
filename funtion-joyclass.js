ed76660701ebbf6e16b60c6d9cbd08275d32d781
Janielson Anjos <72242469+Janielson-Anjos@users.noreply.github.com>
2023-10-12 19:02:41 -0300

    Add files via upload

---
+++ funtion-joyclass.js
@@ -0,0 +1,16 @@
+const readline = require("readline-sync");
+
+
+function tradicional() {
+    
+}
+
+function somar(a, b) {
+    return a + b;
+}
+
+console.log(somar(2,4));
+
+let soma = (a, b)=>a+b;
+
+console.log(soma(5,8));
